e6f7ef9fe303bb9718d8a1b70711d5a9ede78ad7
Gleb <kosia4enko.g@ex.ua>
2019-05-22 15:16:55 +0300

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class MainMenu : MonoBehaviour
+{
+    //public string battleManagementScene;
+    public string creditsScene;
+    public string aiManagementScene;
+
+    public List<Battle> availableMaps;
+    private List<Button> battleButtons;
+
+    [Header("Battle management UI")]
+    
+
+    public GameObject battleListPanel;
+    public GameObject blScrollViewContent;
+    public GameObject blButtonPrefab;
+    public Button blCloseButton;
+
+    public GameObject battlePropertiesPanel;
+    public Text bpMapName;
+    public Button bpStartBtn;
+    public Button bpCloseBtn;
+    public GameObject bpScrollViewContent;
+    public GameObject bpSidePrefab;
+    private const string bpsCountInputField = "Count";
+    private const string bpsAiDropdown = "AI";
+    private const string bpsCodeInputField = "Code";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        battleButtons = new List<Button>(availableMaps.Count);
+
+        foreach (var item in availableMaps)
+        {
+            var btn = Instantiate(blButtonPrefab);
+            var txt = btn.transform.GetChild(0).GetComponent<Text>();
+            txt.text = item.mapName;
+            var btncmp = btn.GetComponent<Button>();
+            var inx = battleButtons.Count;
+            btncmp.onClick.AddListener(() => {
+                OpenBattleProperties(inx);
+            });
+            btn.transform.SetParent(blScrollViewContent.transform);
+            battleButtons.Add(btncmp);
+        }
+
+        blCloseButton.onClick.AddListener(CloseBattleList);
+        bpStartBtn.onClick.AddListener(StartBattleButtonClick);
+        bpCloseBtn.onClick.AddListener(CloseBattlePropertiesWindow);
+
+        battleListPanel.SetActive(false);
+        battlePropertiesPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int currentBattleInx;
+    private void OpenBattleProperties(int battleIndex)
+    {
+        battlePropertiesPanel.SetActive(true);
+        currentBattleInx = battleIndex;
+        //fill panel content
+        var battle = availableMaps[battleIndex];
+        bpMapName.text = string.Format("Map: \"{0}\":", battle.mapName);
+
+        //BAD: it is not that smart to destroy all the gameobjects and than instantiate same. Not only is it a performance issue, it also kills all the data user may has entered.
+        List<GameObject> finalSolutionCandidates = new List<GameObject>();
+        for(int i=0; i<bpScrollViewContent.transform.childCount; i++)
+        {
+            finalSolutionCandidates.Add(bpScrollViewContent.transform.GetChild(i).gameObject);
+        }
+        foreach (var item in finalSolutionCandidates)
+        {
+            Destroy(item);
+        }
+
+        foreach (var item in battle.sides)
+        {
+            var side = Instantiate(bpSidePrefab);
+            side.transform.SetParent(bpScrollViewContent.transform);
+        }
+    }
+
+    public void StartBattleButtonClick()
+    {
+        var battle = availableMaps[currentBattleInx];
+        for (int i = 0; i < battle.sides.Count && i < bpScrollViewContent.transform.childCount; i++)
+        {
+            var side = bpScrollViewContent.transform.GetChild(i);
+            var count = side.transform.Find(bpsCountInputField).GetComponent<InputField>().text;
+            var aiInx = side.transform.Find(bpsAiDropdown).GetComponent<Dropdown>().value;
+            string code = side.transform.Find(bpsCodeInputField).GetComponent<InputField>().text;
+
+            battle.sides[i].count = int.Parse(count);
+            battle.sides[i].aiDifficulty = (Battle.Side.LAID)aiInx;
+            battle.sides[i].code = code;
+        }
+
+        var prev = SceneManager.GetActiveScene().GetRootGameObjects();
+        foreach (var item in prev)
+        {
+            if (item.name == BattleInfo.infoGameObjectName)
+                Destroy(item);
+        }
+
+        var preserver = new GameObject();
+        preserver.name = BattleInfo.infoGameObjectName;
+
+        
+
+        var pb = preserver.AddComponent<BattleInfo>();
+        pb.battle = battle;
+        DontDestroyOnLoad(preserver);
+
+        OpenScene(battle.loadSceneName);
+    }
+    public void CloseBattlePropertiesWindow()
+    {
+        battlePropertiesPanel.SetActive(false);
+    }
+
+    public void OpenBattleList()
+    {
+        battleListPanel.SetActive(true);
+    }
+    public void CloseBattleList()
+    {
+        battleListPanel.SetActive(false);
+    }
+
+    public void OpenAIManagement()
+    {
+        OpenScene(aiManagementScene);
+    }
+    public void OpenOptions()
+    {
+        //TODO: implement options
+    }
+    public void OpenCredits()
+    {
+        OpenScene(creditsScene);
+    }
+
+    private void OpenScene(string sceneName)
+    {
+        if(!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.Log($"coming soon!");
+        }
+    }
+    
+}
+
+[System.Serializable]
+public class Battle
+{
+    [System.Serializable]
+    public class Side
+    {
+        public int count;
+        public enum LAID
+        {
+            disabled, easy, medium, expert, userDefined
+        }
+        public LAID aiDifficulty;
+        public string code;
+        //public Color color;
+    }
+    public string loadSceneName;
+    public string mapName;
+    public List<Side> sides;
+}
+
+public class BattleInfo: MonoBehaviour
+{
+    public const string infoGameObjectName = "ChosenBattle";
+    public Battle battle;
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jurassic;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Tank : MonoBehaviour
+{
+    public InputField code;
+    public bool execute = false;
+
+    [Header("Actuators")]
+    public float speed = 5f;
+    public float tankWidth = 2f;
+    //public float goCenterHeight = 1.5f;
+    public float rotationTreshold = 0.05f;
+    public float turretAngularSpeed = 20f;
+    public float radarAngularSpeed = 60f;
+    public float heatPerShot = 0.2f; //heat treshold = 1f
+    public float overheatFine = 0.2f;
+
+    public Actions actions;
+
+    private ScriptEngine engine = new ScriptEngine();
+    private float initTime = 0f;   
+
+    [System.Serializable]
+    public class Actions
+    {
+        [Range(-0.5f, 1f)]
+        public float leftTrackSpeed;
+        [Range(-0.5f, 1f)]
+        public float rightTrackSpeed;
+        [Range(-1f, 1f)]
+        public float towerAngularSpeed;
+        [Range(-1f, 1f)]
+        public float radarAngularSpeed;
+        public bool fireAtWill;
+
+        public void Validate()
+        {
+            leftTrackSpeed = Mathf.Clamp(leftTrackSpeed, -0.5f, 1f);
+            rightTrackSpeed = Mathf.Clamp(rightTrackSpeed, -0.5f, 1f);
+            towerAngularSpeed = Mathf.Clamp(towerAngularSpeed, -1f, 1f);
+            radarAngularSpeed = Mathf.Clamp(radarAngularSpeed, -1f, 1f);
+        }
+    }
+
+    [System.Serializable]
+    public class Sensors
+    {
+        public enum ObjectCategory
+        {
+            Wall, Ground, Enemy, Ally, Neutral
+        }
+        public float azimuth;
+        //public 
+    }
+
+    public void ExecOnce()
+    {
+        engine.Execute(code.text);
+    }
+
+    Rigidbody rb;
+    // Start is called before the first frame update 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        engine.EnableExposedClrTypes = true;
+        System.Action<string> logger = (s) => Debug.Log(s);
+        engine.SetGlobalFunction("log", logger);
+        engine.SetGlobalValue("actions", actions);
+
+        initTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //    Debug.Break();
+    }
+
+    private void FixedUpdate()
+    {
+        engine.SetGlobalValue("sensors", new Sensors() { azimuth = transform.rotation.eulerAngles.y });
+        
+        //TODO: Validation of actions values;
+        actions.Validate();
+
+        if (execute)
+            engine.Execute(code.text);
+        Movement_PhysicsMess();
+    }
+
+    private void Movement_PhysicsMess()
+    {
+        if (Mathf.Abs(actions.leftTrackSpeed - actions.rightTrackSpeed) < rotationTreshold)
+        {
+            rb.AddForce(transform.forward * 2 * speed * Mathf.Min(actions.leftTrackSpeed, actions.rightTrackSpeed));
+        }
+        else
+        {
+            var force = transform.forward * speed * actions.leftTrackSpeed;
+            var point = transform.TransformPoint(Vector3.left * tankWidth * 0.5f/* + Vector3.down * goCenterHeight*/);
+            rb.AddForceAtPosition(force, point, ForceMode.Force);
+            //Debug.Log($"Force: {force.magnitude}; dist: {(gameObject.transform.position - point).magnitude}");
+            force = transform.forward * speed * actions.rightTrackSpeed;
+            point = transform.TransformPoint(Vector3.right * tankWidth * 0.5f/* + Vector3.down * goCenterHeight*/);
+            rb.AddForceAtPosition(force, point, ForceMode.Force);
+            //Debug.Log($"Force: {force.magnitude}; dist: {(gameObject.transform.position - point).magnitude}");
+            //Debug.LogWarning("Hell!");
+        }
+    }
+
+
+
+    #region Deprecated
+    //private void Movement()
+    //{
+    //    var difference = Mathf.Abs(leftTrackSpeed - rightTrackSpeed);
+    //    float translation = Mathf.Abs(leftTrackSpeed) > Mathf.Abs(rightTrackSpeed) ? leftTrackSpeed : rightTrackSpeed;
+    //    translation -= difference * 0.5f * (translation < 0f ? -1f : 1f);
+
+    //    float alpha = Mathf.Atan(difference * speed / tankWidth) * Mathf.Rad2Deg;
+    //    //var beta = Mathf.Atan()
+    //    //rb.ang
+    //    if (rightTrackSpeed > leftTrackSpeed)
+    //        alpha *= -1;
+
+    //    //transform.Translate(transform.forward * translation * Time.deltaTime * speed, Space.World);
+    //    rb.AddForce(transform.forward * translation * (speed), ForceMode.Force);
+    //    rb.AddTorque(Quaternion.AngleAxis(alpha, transform.up).eulerAngles, ForceMode.Force);
+    //    //transform.Rotate(new Vector3(0f, alpha * Time.deltaTime));
+    //}
+
+    //private void KinematicUpdate()
+    //{
+    //    var difference = Mathf.Abs(leftTrackSpeed - rightTrackSpeed);
+    //    float translation = Mathf.Abs(leftTrackSpeed) > Mathf.Abs(rightTrackSpeed) ? leftTrackSpeed : rightTrackSpeed;
+    //    translation -= difference * 0.5f * (translation < 0f ? -1f : 1f);
+
+    //    float alpha = Mathf.Atan(difference * speed / tankWidth) * Mathf.Rad2Deg;
+    //    if (rightTrackSpeed > leftTrackSpeed)
+    //        alpha *= -1;
+
+    //    transform.Translate(transform.forward * translation * Time.deltaTime * speed, Space.World);
+    //    transform.Rotate(new Vector3(0f, alpha * Time.deltaTime));
+    //}
+    #endregion
+}
