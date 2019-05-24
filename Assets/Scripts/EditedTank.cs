a0863943183ad16e41f56949dc2a3fcb2b9c6488
Gleb <kosia4enko.g@ex.ua>
2019-05-24 23:22:23 +0300

    Started implementing script file IO

--- Assets/Scripts/EditedTank.cs
+++ Assets/Scripts/EditedTank.cs
@@ -1,13 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EditedTank : Tank
 {
+    public const string scriptNames = "scriptNames";
+    public const char scriptSeparator = ';';
+    public const string scriptExtention = ".js";
+
     [Header("UI")]
-    public TMPro.TMP_InputField codeField;
-    public TMPro.TMP_InputField logField;
+    public TMP_InputField codeField;
+    public TMP_InputField logField;
+    public Button saveAsButton;
+    public TMP_InputField saveAsNameField;
+    public Button loadButton;
+    public TMP_Dropdown loadDropdown;
+
+    
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -15,6 +28,69 @@ public class EditedTank : Tank
         this.logger = new UserLogger(logField);
         System.Action<string> logger = this.logger.Log;
         engine.SetGlobalFunction("log", logger);
+
+        saveAsButton.onClick.AddListener(SaveScript);
+    }
+
+
+    private bool IsValidFileName(string s)
+    {
+        const string taboo = ";?,.";
+        var ret = !string.IsNullOrEmpty(s);
+        if (ret)
+        {
+            foreach (var c in s)
+            {
+                if(taboo.Contains(c.ToString()))
+                {
+                    ret = false;
+                    break;
+                }
+            }
+        }
+
+        if (ret)
+        {
+            try
+            {
+                var names = PlayerPrefs.GetString(scriptNames).Split(scriptSeparator).ToList();
+                names.RemoveAll((toTest) => { return string.IsNullOrEmpty(toTest); });
+                foreach (var name in names)
+                {
+                    if (s.Equals(name, System.StringComparison.Ordinal))
+                    {
+                        //TODO: check if string comparison works properly
+                        ret = false;
+                        break;
+                    }
+                }
+            }
+            catch (PlayerPrefsException)
+            {
+                ;
+            }
+        }
+
+        return ret;
+    }
+    public void SaveScript()
+    {
+        var n = saveAsNameField.text;
+        if (IsValidFileName(n))
+        {
+            string names = "";
+            if(PlayerPrefs.HasKey(scriptNames))
+            {
+                names = PlayerPrefs.GetString(scriptNames);
+            }
+            PlayerPrefs.SetString(scriptNames, names + n + scriptSeparator.ToString());
+
+            File.WriteAllText(n + scriptExtention, codeField.text);
+        }
+        else
+        {
+            logger.Log("Set name is not valid either because it is already used or it contains taboo chars. Try other name.");
+        }
     }
 
     public void ExecOnce()
@@ -44,3 +120,5 @@ public class EditedTank : Tank
         }
     }
 }
+
+
