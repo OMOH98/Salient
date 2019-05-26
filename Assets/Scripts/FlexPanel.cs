0b396516c1363b9becf6c7cf436f76a5ad5aeb20
Gleb <kosia4enko.g@ex.ua>
2019-05-26 13:20:11 +0300

    Made Sidebar flexpanel. Flexpanels now saves sate of width and alignment to playerPrefs.

--- Assets/Scripts/FlexPanel.cs
+++ Assets/Scripts/FlexPanel.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+//using System.Runtime.Serialization;
+//using System.Runtime.Serialization;
+
 
 [RequireComponent(typeof(RectTransform))]
 public class FlexPanel : MonoBehaviour
 {
 
-    [Range(0f, 1f)]
-    public float widthCoef=1f;
-    [Range(0f, 1f)]
-    public float heightCoef=1f;
-    public FlexAlignment _alignment = FlexAlignment.left;
-    
+    //[Range(0f, 1f)]
+    //public float widthCoef=1f;
+    //[Range(0f, 1f)]
+    //public float heightCoef=1f;
+    //public FlexAlignment _alignment = FlexAlignment.left;
+
+    public Data data;
+    public uint id; //id unique to all flexPanels
+
     [EnumFlags]
     public FlexAlignment allowedAlignmens;// = FlexAlignment.left|FlexAlignment.right;
     public float paddingLeft, paddingRight;
     public bool changeableWidth = true;
-    public bool changeableHeight = false;
+    public float minWidth = 0.2f;
+    public float maxWidth = 1f;
+    //public bool changeableHeight = false;
     public bool allowHide = true;
 
     public GameObject buttonPrefab;
@@ -38,22 +47,30 @@ public class FlexPanel : MonoBehaviour
 
     public FlexAlignment alignment
     {
-        get { return _alignment; }
+        get { return data.alignment; }
         set
         {
-            if(allowedAlignmens<0||(value&allowedAlignmens)!=0)
+            if (allowedAlignmens < 0 || (value & allowedAlignmens) != 0)
             {
-                _alignment = value;
+                data.alignment = value;
                 Align();
             }
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
         rt = GetComponent<RectTransform>();
         rt.anchorMin = Vector2.zero;
         rt.anchorMax = Vector2.one;
+        BakeIds();
+
+        var k = prefsKey;
+        if(PlayerPrefs.HasKey(k))
+        {
+            data = JsonUtility.FromJson<Data>(PlayerPrefs.GetString(k));
+        }
 
         Align();
 
@@ -81,7 +98,7 @@ public class FlexPanel : MonoBehaviour
             LayoutRebuilder.ForceRebuildLayoutImmediate(trayButtonsParent as RectTransform);
             trayButton.gameObject.SetActive(false);
         }
-        if(allowedAlignmens!=0&&allowedAlignmens!=_alignment)
+        if(allowedAlignmens!=0&&allowedAlignmens!=data.alignment)
         {
             switchAlignmenButton = Instantiate(buttonPrefab).GetComponent<Button>();
             switchAlignmentCaption = switchAlignmenButton.GetComponentInChildren<Text>();
@@ -94,17 +111,18 @@ public class FlexPanel : MonoBehaviour
                     switchAlignmentCaption.text = "Align <";
                 else switchAlignmentCaption.text = "Align >";
                 alignment = a;
+                SaveConfiguration();
             });
         }
         if(changeableWidth)
         {
             widthSlider = Instantiate(widthSliderPrefab).GetComponent<Slider>();
-            widthSlider.minValue = 0f;
-            widthSlider.maxValue = 1f;
-            widthSlider.value = widthCoef;
+            widthSlider.minValue = minWidth;
+            widthSlider.maxValue = maxWidth;
+            widthSlider.value = data.widthCoef;
             widthSlider.onValueChanged.AddListener((v) =>
             {
-                widthCoef = v;
+                data.widthCoef = v;
                 if (applyWidthCanges==null)
                 {
                     applyWidthCanges = StartCoroutine(DelayActionWhile(Align, () =>
@@ -123,17 +141,17 @@ public class FlexPanel : MonoBehaviour
             LayoutRebuilder.ForceRebuildLayoutImmediate((controlsParent == null ? transform : controlsParent) as RectTransform);
         }
     }
-
+    #region Alignment
     private FlexAlignment NextAllowedAlignment()
     {
         if (allowedAlignmens == 0)
-            return _alignment;
+            return data.alignment;
 
         var values = System.Enum.GetValues(typeof(FlexAlignment)).Cast<FlexAlignment>().ToList();
         values.Sort((lhs, rhs) => { return (int)lhs - (int)rhs; });
         var max = values[values.Count - 1];
 
-        var prospective = (int)_alignment;
+        var prospective = (int)data.alignment;
 
         do
         {
@@ -148,13 +166,14 @@ public class FlexPanel : MonoBehaviour
 
     private void Align()
     {
+        SaveConfiguration();
         float apx = 0f, apy = 0f, sdx = 0f, sdy = 0f, hcoef = 1f;
 
-        if (_alignment == FlexAlignment.left)
+        if (data.alignment == FlexAlignment.left)
         {
             hcoef = -1f;
         }
-        switch (_alignment)
+        switch (data.alignment)
         {
             case FlexAlignment.left:
                 hcoef = -1f;
@@ -169,8 +188,8 @@ public class FlexPanel : MonoBehaviour
                 break;
         }
 
-        apx = hcoef * Screen.width * (1f - widthCoef) * 0.5f;
-        switch (_alignment)
+        apx = hcoef * Screen.width * (1f - data.widthCoef) * 0.5f;
+        switch (data.alignment)
         {
             case FlexAlignment.left:
                 apx += paddingLeft;
@@ -182,13 +201,14 @@ public class FlexPanel : MonoBehaviour
                 break;
         }
 
-        sdx = -Screen.width * (1f - widthCoef);
-        sdy = -Screen.height * (1f - heightCoef);
+        sdx = -Screen.width * (1f - data.widthCoef);
+        sdy = -Screen.height * (1f - data.heightCoef);
 
         rt.anchoredPosition = new Vector2(apx, apy);
         rt.sizeDelta = new Vector2(sdx, sdy);
     }
-
+    #endregion
+    #region DelayCoroutines
     public static IEnumerator DelayAction(float seconds, System.Action action)
     {
         yield return new WaitForSeconds(seconds);
@@ -202,13 +222,63 @@ public class FlexPanel : MonoBehaviour
         action();
         yield break;
     }
+    #endregion
+    #region SavingConfigurationAndIdMaking
+    private string prefsKey { get { return "FlexPanel" + id.ToString(); } }
+    protected virtual void SaveConfiguration()
+    {
+        PlayerPrefs.SetString(prefsKey, JsonUtility.ToJson(data));
+    }
+    private static bool IdMaked = false;
+    private const int IdSeed = 45184;
+    private static void BakeIds()
+    {
+        if (IdMaked)
+            return;
+        IdMaked = true;
 
+        List<uint> usedIds = new List<uint>(16);
+        System.Random r = new System.Random(IdSeed);
+        var rgos = SceneManager.GetActiveScene().GetRootGameObjects();
+        var flexPanels = from o in rgos select o.GetComponentsInChildren<FlexPanel>();
+        foreach (var item in flexPanels)
+        {
+            foreach (var panel in item)
+            {
+                var id = 0u;
+                do
+                {
+                    id = (uint)r.Next();
+                } while (usedIds.Contains(id));
+                panel.id = id;
+                usedIds.Add(id);
+            }
+        }
+        
+    }
+
+    #endregion
+
+
+    #region NestedDeclarations
     [System.Flags]
     public enum FlexAlignment
     {
         //nothing - 0, everything =-1
         left = 1, middle = 2, right = 4
     }
+    [System.Serializable]
+    public class Data
+    {
+        public FlexAlignment alignment;
+        [Range(0f, 1f)]
+        public float widthCoef;
+        [Range(0f, 1f)]
+        public float heightCoef;
+    }
+    #endregion
+
+
 }
 
 public class EnumFlagsAttribute : PropertyAttribute
