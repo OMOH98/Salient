76857c355c48c79bdbd1b65300f0111d571b690b
Gleb <kosia4enko.g@ex.ua>
2019-05-26 14:52:19 +0300

    Wired up UI in DerivedBlackTrack

--- Assets/Scripts/EditedTank.cs
+++ Assets/Scripts/EditedTank.cs
@@ -225,7 +225,7 @@ public class EditedTank : MonoBehaviour
         }));
     }
 
-    protected new void StaticStart()
+    protected void StaticStart()
     {
         
         if (PlayerPrefs.HasKey(autosavedScript))
