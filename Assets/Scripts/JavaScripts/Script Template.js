31075bcdf3d92528d5904542023785953839129a
Gleb <kosia4enko.g@ex.ua>
2019-05-27 07:49:03 +0300

    Added callbacks to script template

--- Assets/Scripts/JavaScripts/Script Template.js
+++ Assets/Scripts/JavaScripts/Script Template.js
@@ -5,5 +5,31 @@ function setup(){
 
 //called repeatedly every cycle of physics update (approximatly every 0.02 seconds)
 function loop(){
-	;
+	
+}
+
+/**
+ * @param {Number} ammount
+ * @param {Number} relativeAzimuthToSource
+ */
+function onDamageTaken(ammount, relativeAzimuthToSource){
+	
+}
+
+/**
+ * @param {Number} ammount
+ * @param {Number} relativeAzimuth
+ * @param {String} category
+ */
+function onProximityEnter(distance, relativeAzimuth, category){
+	
+}
+
+/**
+ * @param {Number} ammount
+ * @param {Number} relativeAzimuth
+ * @param {String} category
+ */
+function onCollisionEnter(distance, relativeAzimuth, category){
+	
 }
