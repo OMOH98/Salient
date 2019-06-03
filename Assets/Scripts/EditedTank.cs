c00882f6af3ef93188e099275fe2186b8650d586
Gleb <kosia4enko.g@ex.ua>
2019-06-03 12:33:50 +0300

    Extracted CR clearer

--- Assets/Scripts/EditedTank.cs
+++ Assets/Scripts/EditedTank.cs
@@ -62,55 +62,6 @@ public class EditedTank : MonoBehaviour
     }
 
 
-    public class ClearScriptCR : MonoBehaviour
-    {
-        public int treshold = 3;
-
-        const string text = "text";
-        int prevLength = 0;
-        
-        dynamic tmpField;
-        //System.Reflection.PropertyInfo textProp;
-        private void Start()
-        {
-            var components = (from c in GetComponents(typeof(Component))
-                       where c.GetType().GetProperties().ToList().Find((inf) =>
-                       {
-                           return inf.CanRead&&inf.CanWrite && inf.PropertyType == typeof(string) && inf.Name == text;
-                       }) != null
-                       select c).ToArray();
-            if (components.Length > 0)
-            {
-                tmpField = components[0];
-                //textProp = tmpField.GetType().GetProperty(text);
-            }
-        }
-
-        bool works = true;
-        private void Update()
-        {
-            if (!works)
-                return;
-            if(tmpField==null)
-            {
-                Debug.LogWarning($"No component with text property was found on {gameObject.name} game object");
-                works = false;
-                return;
-            }
-
-            if (tmpField != null && tmpField.text.Length - prevLength > treshold)
-            {
-                tmpField.text = ClearCR(tmpField.text);
-            }
-            prevLength = tmpField.text.Length;
-        }
-        public static string ClearCR(string input)
-        {
-            var sb = new StringBuilder(input);
-            sb.Replace("\r", "");
-            return sb.ToString();
-        }
-    }
 
     #region ScriptIO
     public static List<string> GetScriptNames()
@@ -308,7 +259,7 @@ public class EditedTank : MonoBehaviour
 
     protected void StaticStart()
     {
-        codeField.gameObject.AddComponent<ClearScriptCR>();
+        //codeField.gameObject.AddComponent<ClearScriptCR>();
         if (PlayerPrefs.HasKey(autosavedScript))
         {
             var lastCode = PlayerPrefs.GetString(autosavedScript);
