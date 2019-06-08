7ab387aab83d74a9a2b5ef5e2431e89d88c896ea
Gleb <kosia4enko.g@ex.ua>
2019-06-08 23:42:51 +0300

    added Post processing to workscene

---
+++ Assets/Scripts/ShortCrutches/DrawALine.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DrawALine : MonoBehaviour
+{
+    public GameObject destination;
+
+    LineRenderer lr;
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.positionCount = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, destination.transform.position);
+    }
+}
