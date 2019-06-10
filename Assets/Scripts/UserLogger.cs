dd2bf8ab7afa93ba149d51d1861c6745dc3aa76d
Gleb <kosia4enko.g@ex.ua>
2019-06-10 20:56:35 +0300

    Simplified log UI update

--- Assets/Scripts/UserLogger.cs
+++ Assets/Scripts/UserLogger.cs
@@ -11,39 +11,58 @@ using TMPro;
 public class UserLogger : MonoBehaviour, Tank.Logger
 {
     public InputField partialLog;
+
     public TMP_InputField fullLog;
-    public int maxSymbolsToDisplay = 1000;
+
+    //public int maxSymbolsToDisplay = 1000;
+    public int messagesToDisplay = 10;
+
+    public const string logUpdateFrequency = nameof(logUpdateFrequency);
+
 
     private List<string> messages = new List<string>();
     private float initialTime;
+    private float updatePeriod;
     private int previousCount = 0;
     private StringBuilder sb;
 
     void Start()
     {
         partialLog.readOnly = true;
-        sb = new StringBuilder();
         initialTime = Time.time;
-        previousCount = 0;
+        sb = new StringBuilder();
 
-        fullLog.text = "";
+        float up;
+        if (Options.TryGetOption(logUpdateFrequency, out up))
+            updatePeriod = up;
+        if (fullLog != null)
+            fullLog.text = "";
     }
 
+    float nextTimeToUpdate = 0f;
     private void Update()
     {
-        if(messages.Count>previousCount)
+        if(Time.time >= nextTimeToUpdate)
         {
-            sb.Clear();
-            sb.Append(partialLog.text);
-            for (int i = previousCount; i < messages.Count; i++)
+            if (fullLog != null)
             {
-                fullLog.text += string.Format("[Time: {0:F1}] {1}\n", Time.time - initialTime, messages[i]);
+                sb.Clear();
+                for (int i = previousCount; i < messages.Count; i++)
+                {
+                    sb.AppendFormat("[Time: {0:F1}] {1}\n", Time.time - initialTime, messages[i]);
+                }
+                fullLog.text += sb.ToString();
+                previousCount = messages.Count;
+            }
+
+            sb.Clear();
+            for (int i = messages.Count-1; i>= 0 && i > messages.Count - messagesToDisplay; i--)
+            {
                 sb.Insert(0, messages[i] + "\n-------------------------------------------------------\n");
             }
-            previousCount = messages.Count;
-            if (sb.Length > maxSymbolsToDisplay)
-                sb.Remove(maxSymbolsToDisplay, sb.Length - maxSymbolsToDisplay);
             partialLog.text = sb.ToString();
+
+            nextTimeToUpdate = Time.time + updatePeriod;
         }
     }
 
