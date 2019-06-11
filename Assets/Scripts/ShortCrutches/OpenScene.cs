e6ed32d00ba1baf53893591852bc8e8d3492f6a7
Gleb <kosia4enko.g@ex.ua>
2019-06-11 08:32:42 +0300

    Return to menu button in arena scene

---
+++ Assets/Scripts/ShortCrutches/OpenScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenScene : MonoBehaviour
+{
+    public string SceneName = "Menu";
+    public void Open()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
